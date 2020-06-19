39cb45e2c322348f9bb4d8bdf67ae99b004af7c3
zhe <zhe.ji@carleton.ca>
2020-06-19 01:34:52 -0400

    Code clean up.

--- src/main/java/com/Model/User.java
+++ src/main/java/com/Model/User.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Collection;
