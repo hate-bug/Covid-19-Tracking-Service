89b83c33441300cc28850ad332e0fe4cbb11ebc3
zhe <zhe.ji@carleton.ca>
2020-06-25 22:24:45 -0400

    Fix heroku

--- src/main/java/com/Configuration/RedisConfig.java
+++ src/main/java/com/Configuration/RedisConfig.java
@@ -1,8 +1,10 @@
 package com.Configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.session.data.redis.config.ConfigureRedisAction;
 
+@Configuration
 public class RedisConfig {
     @Bean
     ConfigureRedisAction configureRedisAction() {
